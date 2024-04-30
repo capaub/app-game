af22df15e3e043c32273ce76f2275e1213b0beea
anissaabenn <benboina.anissa@outlook.fr>
2024-04-30 14:30:15 +0200

    Class chrono

--- msQuizz/src/main/java/com/capaub/msQuizz/MsQuizzApplication.java
+++ msQuizz/src/main/java/com/capaub/msQuizz/MsQuizzApplication.java
@@ -1,13 +1,20 @@
 package com.capaub.msQuizz;
 
+import com.capaub.msQuizz.model.CustomTimer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 @SpringBootApplication
 public class MsQuizzApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsQuizzApplication.class, args);
+
+		Timer chrono = new Timer();
+		chrono.schedule(new CustomTimer(), 2000, 1000);
 	}
 
 }
