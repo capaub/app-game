71a81d7d65341c4d1f9bbaba5b21af43aa9a394b
aubry <aubry@exemple.com>
2024-05-01 11:52:09 +0200

    group import annotation.* // adding the @CrossOrigin annotation to allow the call from Angular

--- msQuizz/src/main/java/com/capaub/msQuizz/controller/QuizzController.java
+++ msQuizz/src/main/java/com/capaub/msQuizz/controller/QuizzController.java
@@ -1,12 +1,11 @@
 package com.capaub.msQuizz.controller;
 
 import com.capaub.msQuizz.service.QuizzService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+// pour autoriser l'appel d'angular
+@CrossOrigin(origins = "http://localhost:4200")
 public class QuizzController {
     private final QuizzService quizzService;
 
