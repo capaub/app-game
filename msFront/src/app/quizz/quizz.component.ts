b5ec125d644d96d15bca6cdea06fe0dcb29aaa12
aubry <aubry@exemple.com>
2024-05-03 16:51:18 +0200

    add counter

--- msFront/src/app/quizz/quizz.component.ts
+++ msFront/src/app/quizz/quizz.component.ts
@@ -1,21 +1,22 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {QuizzModel} from "../model/Quizz.model";
+import {AppComponent} from "../app.component";
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
-export class QuizzComponent {
+export class QuizzComponent extends AppComponent{
   @Input() quizz!: QuizzModel;
   @Output() public send = new EventEmitter();
   @Output() public nextIndex = new EventEmitter();
 
-  index = 0;
 
-  sendResponse(questionId: number, question: string, responseId: number, response: string) {
-    this.send.emit({questionId, question, responseId, response})
-    this.nextIndex.emit(this.index + 1)
+  sendResponse(questionId: number, responseId: number,quizz:QuizzModel) {
+    this.send.emit({questionId, responseId});
+    this.nextIndex.emit(this.index + 1);
     this.index += 1;
+    this.countPoint(responseId,quizz);
   }
 }
