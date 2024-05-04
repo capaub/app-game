5c77f4631d0f3aa1868bb7b96260fa43f61c19af
aubry <aubry@exemple.com>
2024-05-04 18:56:24 +0200

    clean code

--- msFront/src/app/quizz/quizz.component.ts
+++ msFront/src/app/quizz/quizz.component.ts
@@ -1,32 +1,23 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {QuizzModel} from "../model/Quizz.model";
 import {AppComponent} from "../app.component";
+import {Label} from "../model/Label.model";
 
 @Component({
   selector: 'app-quizz',
   templateUrl: './quizz.component.html',
   styleUrl: './quizz.component.css'
 })
+
 export class QuizzComponent extends AppComponent {
   @Input() quizz!: QuizzModel;
   @Output() public send = new EventEmitter();
   @Output() public nextIndex = new EventEmitter();
-
 
   sendResponse(questionId: number, responseId: number, quizz: QuizzModel) {
     this.send.emit({questionId, responseId});
     this.nextIndex.emit(this.index + 1);
     this.index += 1;
     this.countPoint(responseId, quizz);
-  }
-
-
-  //affichage composant choix level-theme:
-  isClick(event: Event) {
-    let element = event.currentTarget
-    // @ts-ignore
-    element.classList.toggle("selected")
-    //let elts= document.querySelectorAll(".btn")
-    //for(let i =0; i<elts.length, i++)
   }
 }
