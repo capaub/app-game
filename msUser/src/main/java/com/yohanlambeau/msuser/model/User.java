aed1bf134e8b3cc8df3e5bdeed1417e56fdcd3fe
aubry <aubry@exemple.com>
2025-08-28 12:35:39 +0200

    Last update / clean code

--- msUser/src/main/java/com/yohanlambeau/msuser/model/User.java
+++ msUser/src/main/java/com/yohanlambeau/msuser/model/User.java
@@ -1,9 +1,13 @@
 package com.yohanlambeau.msuser.model;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
-public class User {
+public class User implements UserDetails {
     @Id
     private int idUser;
     private String pseudo;
@@ -44,8 +48,38 @@ public class User {
         this.mail = mail;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 
     public void setPassword(String password) {
--- msUser/src/main/java/com/yohanlambeau/msuser/service/UserService.java
+++ msUser/src/main/java/com/yohanlambeau/msuser/service/UserService.java
@@ -3,18 +3,19 @@ package com.yohanlambeau.msuser.service;
 import com.yohanlambeau.msuser.model.User;
 import com.yohanlambeau.msuser.repository.UserRepository;
 import com.yohanlambeau.msuser.service.form.SignUpForm;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
+
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
 
     public void registration(SignUpForm form) {
         User user = new User();
@@ -23,4 +24,11 @@ public class UserService {
         user.setPassword(passwordEncoder.encode(form.getPassword()));
         userRepository.save(user);
     }
+
+   @Override
+   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+       return this.userRepository
+               .findUserByMail(username)
+               .orElseThrow(()->new UsernameNotFoundException("non trouvé"));
+   }
 }
